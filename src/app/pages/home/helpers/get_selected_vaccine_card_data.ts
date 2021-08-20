2c01cfa3f8314e62b921bc866e943737bc9b57f4
Joseph Chingalo <profschingalo@gmail.com>
2021-08-20 12:08:54 +0300

    chore: helpers for extraction of vaccine card data

---
+++ src/app/pages/home/helpers/get_selected_vaccine_card_data.ts
@@ -0,0 +1,119 @@
+import * as _ from "lodash";
+import {
+  VACCINE_CARD_DOSES_HEADERS,
+  VACCINE_CARD_LOCATION,
+  VACCINE_CARD_BIO,
+} from "src/app/core/configs/vaccine-dose-headers";
+import {
+  VaccinationCard,
+  VaccinationCardHeader,
+} from "src/app/core/models/vaccination-card";
+
+export function getVaccineCardBioData(
+  selectedVaccinationCard: VaccinationCard
+) {
+  return _.map(VACCINE_CARD_BIO, (bioDataRow: any) => {
+    return _.map(bioDataRow, (bioData: any) => {
+      const { name, ids } = bioData;
+      const value = _.join(
+        _.filter(
+          _.flattenDeep(
+            _.map(ids, (id: string) => {
+              const cardData = _.find(
+                selectedVaccinationCard.headers,
+                (data: VaccinationCardHeader) => data?.id === id
+              );
+              return cardData && cardData.value ? cardData.value : "";
+            })
+          ),
+          (value: string) => value != ""
+        ),
+        " "
+      );
+      console.log(name, ids);
+      return { ...bioData, value };
+    });
+  });
+}
+
+export function getVaccineCardBioLocation(
+  selectedVaccinationCard: VaccinationCard
+) {
+  return _.join(
+    _.filter(
+      _.flattenDeep(
+        _.map(VACCINE_CARD_LOCATION, (id: string) => {
+          const cardData = _.find(
+            selectedVaccinationCard.headers,
+            (data: VaccinationCardHeader) => data?.id === id
+          );
+          return cardData && cardData.value ? cardData.value : "";
+        })
+      ),
+      (value: string) => value != ""
+    ),
+    " > "
+  );
+}
+
+export function getDoseIndexes(selectedVaccinationCard: VaccinationCard) {
+  return _.sortedUniq(
+    _.flattenDeep(
+      _.map(
+        _.filter(
+          selectedVaccinationCard.headers || [],
+          (headerConfig: VaccinationCardHeader) =>
+            headerConfig.hasOwnProperty("doseIndex")
+        ),
+        (headerConfig: VaccinationCardHeader) => headerConfig.doseIndex
+      )
+    )
+  );
+}
+
+export function getDosesHeaderValues(selectedVaccinationCard: VaccinationCard) {
+  const vaccinaDoseHeaderValues = [];
+  for (const dataId of VACCINE_CARD_DOSES_HEADERS) {
+    const data = _.find(
+      selectedVaccinationCard.headers,
+      (dataObject: VaccinationCardHeader) => dataObject?.id === dataId
+    );
+    const value = data ? data.name || "" : "";
+    vaccinaDoseHeaderValues.push(value);
+  }
+  return vaccinaDoseHeaderValues;
+}
+
+export function getDosesRowValues(
+  doses: any,
+  selectedVaccinationCard: VaccinationCard
+) {
+  const vaccinaDoseRowValues = [];
+  for (const doseIndex in doses) {
+    const vaccinaDoseRow = [];
+    const selectedDose: Array<VaccinationCardHeader> = _.filter(
+      selectedVaccinationCard.headers || [],
+      (headerConfig: VaccinationCardHeader) =>
+        headerConfig.hasOwnProperty("doseIndex") &&
+        `${headerConfig.doseIndex}` === `${doseIndex}`
+    );
+    const hasSelectedDoseEmpty =
+      _.flattenDeep(
+        _.map(selectedDose, (data: any) =>
+          data?.value !== "" ? data.value : []
+        )
+      ).length === 0;
+    if (!hasSelectedDoseEmpty) {
+      for (const dataId of VACCINE_CARD_DOSES_HEADERS) {
+        const data = _.find(
+          selectedDose,
+          (dataObject: VaccinationCardHeader) => dataObject?.id === dataId
+        );
+        const value = data ? data.value || "" : "";
+        vaccinaDoseRow.push(value);
+      }
+      vaccinaDoseRowValues.push(vaccinaDoseRow);
+    }
+  }
+  return vaccinaDoseRowValues;
+}
