d9c1a21e1cc9cb5ef3131513d0518006b4c90a58
Joseph Chingalo <profschingalo@gmail.com>
2021-08-20 12:08:25 +0300

    feat: config for vaccine card

---
+++ src/app/core/configs/vaccine-dose-headers.ts
@@ -0,0 +1,28 @@
+export const VACCINE_CARD_DOSES_HEADERS = [
+  "bbnyNYD1wgS",
+  "Yp1F4txx8tm",
+  "LUIsbsm3okG",
+  "eventDate",
+  "eRi9ifg9v5h",
+];
+
+export const VACCINE_CARD_LOCATION = ["state", "country", "payam"];
+
+export const VACCINE_CARD_BIO = [
+  [
+    { name: "Identification number", ids: ["KSr2yTdu1AI"] },
+    { name: "Full name", ids: ["sB1IHYu2xQT", "ENRjVGxVL6l"] },
+    { name: "Date of birth", ids: ["NI0QRzJvQ0k"] },
+    { name: "Age", ids: ["Rv8WM2mTuS5"] },
+    { name: "Gender", ids: ["oindugucx72"] },
+  ],
+  [
+    { name: "Phone #", ids: ["fctSQp5nAYl"] },
+    { name: "Physical Address", ids: ["Xhdn49gUd52"] },
+    { name: "Occupation", ids: ["LY2bDXpNvS7"] },
+    { name: "History of Allergy", ids: ["dWoveSw6b79"] },
+    { name: "Existing condition", ids: ["bCtWZGjSWM8"] },
+  ],
+];
+
+export const VACCINE_CARD_QRCODE = [];
