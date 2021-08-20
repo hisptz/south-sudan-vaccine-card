5cf078819682e5e23a31f57014d9d38dea4a4fc5
Joseph Chingalo <profschingalo@gmail.com>
2021-08-20 12:12:56 +0300

    feat: uI improvement on vaccine card header as well as add support for ui for qrd code verifications

--- src/app/pages/home/components/vaccine-card-qr-code/vaccine-card-qr-code.component.ts
+++ src/app/pages/home/components/vaccine-card-qr-code/vaccine-card-qr-code.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input } from "@angular/core";
+import * as _ from "lodash";
 
 import { VaccinationCard } from "src/app/core/models/vaccination-card";
+import {
+  getDoseIndexes,
+  getDosesHeaderValues,
+  getDosesRowValues,
+} from "../../helpers/get_selected_vaccine_card_data";
 
 @Component({
   selector: "app-vaccine-card-qr-code",
@@ -15,6 +21,38 @@ export class VaccineCardQrCodeComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.qrdCode = `name : Joseph Changlo\nGender : M\nAge : 30\n\n\nLink : https://github.com/`;
+    if (this.selectedVaccinationCard && this.selectedVaccinationCard.headers) {
+      const doses: any = getDoseIndexes(this.selectedVaccinationCard);
+      const vaccinaDoseRowValues: any = getDosesRowValues(
+        doses,
+        this.selectedVaccinationCard
+      );
+      const vaccinaDoseHeaderValues = getDosesHeaderValues(
+        this.selectedVaccinationCard
+      );
+      const vaccineCardUrl = window?.location?.href || "";
+      const vaccineDose = _.join(
+        _.flattenDeep(
+          _.map(vaccinaDoseRowValues, (vaccinaDoseRow) => {
+            return _.join(
+              _.flattenDeep(
+                _.map(vaccinaDoseHeaderValues, (vaccinaDoseHeader) => {
+                  const index = _.indexOf(
+                    vaccinaDoseHeaderValues,
+                    vaccinaDoseHeader
+                  );
+                  const value = vaccinaDoseRow[index] || "";
+                  return `${vaccinaDoseHeader} : ${value}`;
+                })
+              ),
+              ", "
+            );
+          })
+        ),
+        "\n\n"
+      );
+      this.qrdCode = `name : Joseph Changlo\nGender : M\nAge : 30\n${vaccineDose}`;
+      this.qrdCode += vaccineCardUrl ? `\n\n\n${vaccineCardUrl}` : "";
+    }
   }
 }
