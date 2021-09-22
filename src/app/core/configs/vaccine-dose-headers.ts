1b8e494d3ea8c71be359e098906d24eb7d9407a1
Joseph Chingalo <profschingalo@gmail.com>
2021-09-22 15:25:48 +0300

    chore(config): updating configs for vaccine bio card

--- src/app/core/configs/vaccine-dose-headers.ts
+++ src/app/core/configs/vaccine-dose-headers.ts
@@ -10,11 +10,19 @@ export const VACCINE_CARD_LOCATION = ["state", "country", "payam"];
 
 export const VACCINE_CARD_BIO = [
   [
-    { name: "Identification number", ids: ["KSr2yTdu1AI"] },
-    { name: "Full name", ids: ["sB1IHYu2xQT", "ENRjVGxVL6l"] },
-    { name: "Date of birth", ids: ["NI0QRzJvQ0k"] },
-    { name: "Age", ids: ["Rv8WM2mTuS5"] },
-    { name: "Gender", ids: ["oindugucx72"] },
+    {
+      id: "identification_number",
+      name: "Identification number",
+      ids: ["KSr2yTdu1AI"],
+    },
+    {
+      id: "full_name",
+      name: "Full name",
+      ids: ["sB1IHYu2xQT", "ENRjVGxVL6l", "NfbmVFsS80D"],
+    },
+    { id: "date_of_birth", name: "Date of birth", ids: ["NI0QRzJvQ0k"] },
+    { id: "age", name: "Age", ids: ["Rv8WM2mTuS5"] },
+    { id: "gender", name: "Gender", ids: ["oindugucx72"] },
   ],
   [
     { name: "Phone #", ids: ["fctSQp5nAYl"] },
